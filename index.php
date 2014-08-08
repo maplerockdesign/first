ef5daade44a21cb5eca619fd572885c95944d6a9
maplerockdesign <richard_bell@Richard-Bells-MacBook-Pro.local>
2014-08-08 21:28:52 +0100

    added some actual html with code kit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="less.css">
+<script type="text/javascript" src="rb.js" />
+</head>
+
+<body>
+
+</body>
+</html>
---
+++ min/rb-min.js
@@ -0,0 +1,1 @@
+document.addEvent("domready",function(){console.log("test")});
